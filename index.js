e775422f8ef4b01439eaf4c3199caac57b87ab6b
chelm <christopher.helm@gmail.com>
2014-08-19 14:06:00 -0600

    refactored routes go one

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.name = 'agol';
+exports.pattern = '/:id/:item';
 exports.controller = require('./controller');
 exports.routes = require('./routes');
 exports.model = require('./models/agol.js');
