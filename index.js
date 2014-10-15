35cb100be9e6f9a01a6320ddd190ce567a5e2095
chelm <christopher.helm@gmail.com>
2014-10-15 12:49:10 -0600

    adding status to provider

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var pjson = require('./package.json');
+var git = require('git-rev-sync');
+
 exports.name = 'agol';
 exports.pattern = '/:id/:item';
 exports.controller = require('./controller');
 exports.routes = require('./routes');
 exports.model = require('./models/agol.js');
+exports.status = { sha: git.long(), version: pjson.version, lastCommit: git.log()[0] };
