827f810fd6b84e33097b61dfc68e91619986b6e7
chelm <christopher.helm@gmail.com>
2014-10-22 15:27:44 -0600

    better version output

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var pjson = require('./package.json');
-var git = require('git-rev-sync');
+var fs = require('fs');
 
 exports.name = 'agol';
 exports.pattern = '/:id/:item';
 exports.controller = require('./controller');
 exports.routes = require('./routes');
 exports.model = require('./models/agol.js');
-exports.status = { sha: git.long(), version: pjson.version, lastCommit: git.log()[0] };
+exports.status = { sha: fs.readFileSync( __dirname + '/.git/refs/heads/master' ).toString(), version: pjson.version};
 
