609daed0280c5a415f7c3da6be1f4499984d889d
Daniel Fenton <dfenton@esri.com>
2015-08-13 14:05:33 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,8 @@ module.exports = {
   'get /agol/:id/:item/:layer/tiles/:z/:x/:y.:format': 'tiles',
   'get /agol/:id/:item/:layer/tiles/:z/:x/:y': 'tiles',
   'get /agol/:id/:item/FeatureServer/:layer': 'featureserver',
-  'get /agol/:ite/:item/FeatureServer/:layer/:method': 'featureserver',
-  'post /agol/:ite/:item/FeatureServer/:layer/:method': 'featureserver',
+  'get /agol/:id/:item/FeatureServer/:layer/:method': 'featureserver',
+  'post /agol/:id/:item/FeatureServer/:layer/:method': 'featureserver',
   'get /agol/:id/:item/FeatureServer/:layer/geohash': 'getGeohash'
 }
 
