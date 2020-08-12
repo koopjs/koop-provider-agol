90e7446f83a5b87ebcdd6ed609fbc5f80254370e
Rich <rich.gwozdz@gmail.com>
2020-08-12 08:53:47 -0700

    Cleanup.

--- server.js
+++
@@ -1,9 +0,0 @@
-const config = require('config')
-const Koop = require('koop')
-const koop = new Koop()
-const agol = require('./')
-
-koop.register(agol)
-
-const port = config.port || 8085
-koop.server.listen(port, () => console.log(`Koop listening on port ${port}!`))
